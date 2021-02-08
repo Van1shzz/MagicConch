83bb2c177f2e5ac0f61cf44f1bea6123616fcd35
van1shzz <ppmblast@gmail.com>
2021-02-07 21:52:34 -0500

    working

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+document.title = "Conch Simulator"
 
 function App() {
   return (
+    
     <div className="App">
+      <div>
       <h1>The magic conch! A conch brother</h1>
+      <img src="images/conch.png"></img>
+      </div>
+      <div>
+      <form>
+  <label>
+    Ask your Question:
+    <input type="text" name="question" />
+  </label>
+  <input type="submit" value="Ask" />
+</form>
+      </div>
     </div>
   );
 }
